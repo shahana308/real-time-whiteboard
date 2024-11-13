4d7c52433fc1cfd9b9577972578d7b9aad14952a
Shahana Salam <shahana.salam@cafu.com>
2024-11-13 22:04:56 +0400

    feat: update server

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
