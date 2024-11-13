e41667a61a6e0d7ec9f41a177a36ac943619b84f
Shahana Salam <shahana.salam@cafu.com>
2024-11-13 22:22:48 +0400

    feat: update server

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
-  transports: ["websocket"],
-});
+const socket = io(
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000",
+  {
+    transports: ["websocket"],
+  }
+);
 
 export default socket;
