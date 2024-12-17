191df5deef492d2c475d73271f8c35c789ddb8b2
Shahana Salam <shahana.salam@cafu.com>
2024-12-17 22:24:35 +0400

    feat: remove old tailwind file:

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
+console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
 const socket = io(
   process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000",
   {
